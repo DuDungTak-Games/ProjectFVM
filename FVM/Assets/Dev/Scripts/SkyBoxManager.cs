a406a4b68bd9863474693858f3ed736126a7f487
BUILD <changbeomyoon2002@gmail.com>
2021-08-18 18:42:22 +0900

    자판기 발사 맵 하늘 배경 임시 구성
    - 높이별 자연스러운 그라데이션 색상 변화가 이루어지도록 적용 (임시)
    - 공중에만 있는걸 고려하여 광원 색상을 고정하여 스카이박스 배경색에 따라 광원 변화가 발생하지 않도록 세팅

--- FVM/Assets/Dev/Scripts/SkyBoxManager.cs
+++ FVM/Assets/Dev/Scripts/SkyBoxManager.cs
@@ -1,18 +1,93 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SkyBoxManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Material skyboxMaterialOri;
+
+    public Transform target;
+    
+    public List<ColorSet> colorList;
+
+    private Material skyboxMaterial;
+    
+    private ColorSet curColorSet;
+
+    private float prevHeight = 0;
+    
+    private int curIndex = 0;
+    
+    [Serializable]
+    public struct ColorSet
     {
+        public int targetHeight;
         
+        public Color top;
+        public Color center;
+        public Color bottom;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        skyboxMaterial = RenderSettings.skybox;
+        
+        curColorSet = colorList[curIndex];
+        UpdateColor();
+
+        curIndex++;
+    }
+    
     void Update()
     {
+        UpdateSkyBox();
+    }
+
+    void UpdateSkyBox()
+    {
+        if (curIndex >= colorList.Count || curIndex <= 0)
+            return;
         
+        float curHeight = target.position.y;
+        float targetHeight = colorList[curIndex].targetHeight;
+        
+        if (curHeight < prevHeight)
+        {
+            curIndex--;
+            
+            prevHeight = colorList[curIndex].targetHeight;
+        }
+        else if (curHeight < targetHeight)
+        {
+            float t = (curHeight - prevHeight) / targetHeight;
+
+            curColorSet.top = Color.Lerp(colorList[curIndex-1].top, colorList[curIndex].top, t);
+            curColorSet.center = Color.Lerp(colorList[curIndex-1].center, colorList[curIndex].center, t);
+            curColorSet.bottom = Color.Lerp(colorList[curIndex-1].bottom, colorList[curIndex].bottom, t);
+            
+            UpdateColor();
+        }
+        else if(curHeight >= targetHeight)
+        {
+            prevHeight = targetHeight;
+            
+            curIndex++;
+        }
+    }
+
+    void UpdateColor()
+    {
+        skyboxMaterial.SetColor("_Top", curColorSet.top);
+        skyboxMaterial.SetColor("_Center", curColorSet.center);
+        skyboxMaterial.SetColor("_Bottom", curColorSet.bottom);
+    }
+
+    private void OnApplicationQuit()
+    {
+        RenderSettings.skybox.SetColor("_Top", skyboxMaterialOri.GetColor("_Top"));
+        RenderSettings.skybox.SetColor("_Center", skyboxMaterialOri.GetColor("_Center"));
+        RenderSettings.skybox.SetColor("_Bottom", skyboxMaterialOri.GetColor("_Bottom"));
     }
 }
