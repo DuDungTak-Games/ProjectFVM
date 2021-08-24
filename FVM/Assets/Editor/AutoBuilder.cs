92295bfce73eb8ee666ca82da7da63164267749b
BUILD <changbeomyoon2002@gmail.com>
2021-08-24 20:52:54 +0900

    젠킨스 빌드용 스크립트 세팅 재수정

--- FVM/Assets/Editor/AutoBuilder.cs
+++ FVM/Assets/Editor/AutoBuilder.cs
@@ -7,7 +7,7 @@ using UnityEditor.Build.Reporting;
  
 namespace JENKINS
 {
-    public class AutoBuilder
+    public class AutoBuilder : ScriptableObject
     {
         static string[] SCENES = FindEnabledEditorScenes();
  
